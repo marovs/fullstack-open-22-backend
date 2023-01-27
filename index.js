0bb55e1e520c15a33f5fc2fe9472ebf97e15be49
Martin Øvsttun <marovsttun@gmail.com>
2023-01-27 08:40:43 +0100

    part3b added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express()
 
 app.use(express.json())
@@ -7,6 +8,8 @@ app.use(express.json())
 morgan.token("body", (request) => {
     return JSON.stringify(request.body)
 })
+
+app.use(cors())
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
@@ -90,7 +93,7 @@ const getNewId = () => {
     return Math.floor(Math.random() * 1000000)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
