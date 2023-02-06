c837cb89050f9a77de7a5a2495134c037279e22d
Martin Øvsttun <marovsttun@gmail.com>
2023-02-06 08:53:52 +0100

    part3d phonebook database exercises 3.19-3.21

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ app.delete("/api/persons/:id", (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
 
-    if (!(body.name && body.number)) {
+    if (Person.exists(body.name)) {
         return response.status(400).json({
-            error: "missing name or number"
+            error: "name must be unique"
         })
     }
 
@@ -62,9 +62,11 @@ app.post("/api/persons", (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -75,7 +77,11 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id,
+        person,
+        {new: true, runValidators: true, context: "query"}
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -93,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformed id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -14,8 +14,22 @@ mongoose.connect(url)
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /\d{2}-\d+|\d{3}-\d+|\d+/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number.`
+        }
+    },
 })
 
 personSchema.set("toJSON", {
